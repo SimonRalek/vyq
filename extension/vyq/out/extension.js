c3f39b36f44af02e1f09f31afe04faae754411e0
Simon Ralek <simon.ralek@gmail.com>
2024-03-24 18:30:39 +0100

    Updated extension

--- extension/vyq/out/extension.js
+++ extension/vyq/out/extension.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
 const vscode = require("vscode");
 function activate(context) {
-    console.log('Congratulations, your extension "vyq-language-support" is now active!');
+    console.log('Gratulejeme, vaše rozšíření "Vyq" je nyní aktivní!');
     // let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
     //     vscode.window.showInformationMessage('Hello World from VyQ Language Support!');
     // });
